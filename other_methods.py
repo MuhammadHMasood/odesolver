93d8a3b57caad40757d16819ac9c92805b32a6a5
Muhammad Masood <muhammadhmasood@gmail.com>
2025-08-24 16:13:29 -0700

    scaffold for n-d implicit midpoint

--- other_methods.py
+++ other_methods.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy.typing as npt
 
-from solver import ODEFunc
+from solver import ODEFunc, enforce_1d
 
 
 # supposing 1 stage 1-d implicit method we have
@@ -50,12 +50,169 @@ def implicit_midpoint_1d(y_k, h: np.floating, f: ODEFunc) -> npt.NDArray:
     def g(x):
         return (h / 2) * f(x) + y_k - x
 
-    Y_1 = newtons_method_approx(g, 1e-5, y_k)
+    Y_1 = newtons_method_approx1d(g, 1e-5, y_k)
 
     return y_k + h * f(Y_1)
 
 
-def newtons_method_exact(the_function, the_derivative, iterations, initial_guess):
+# Deriving the solution in n-d instead of 1-d
+
+# If we start in 1-d
+# Our RK method is:
+
+# Y_1 = y_n + a11 * h * f(Y_1)
+
+# with a11 = 1/2
+
+# relabeling Y_1 to x, we get
+
+# (h/2) * f(x) + y_n - x = 0
+
+# let
+
+# g(x) = (h/2) * f(x) + y_n - x
+
+# then we just run newton's method on g(x) to get our value for Y_1 (aka x)
+
+# ---
+
+# Now let's go to n-d with the same method
+
+# Y_1-1 = y_n-1 + (h/2) * f1(Y_1)
+# Y_1-2 = y_n-2 + (h/2) * f2(Y_1)
+# ...
+# Y_1-d = y_n-d + (h/2) * fd(Y_1)
+
+# or vectorially
+
+# Y_1 = y_n + (h/2) * f(Y_1)
+
+# then if we define (relabelling Y_1 as x
+
+# gi(x) = (h/2) * fi(x) + y_n-i - x-i
+
+# then we want to use newton's method on the system:
+
+# g1(x) = 0
+# g2(x) = 0
+# ...
+# gd(x) = 0
+
+# or vectorially g(x) = 0
+
+# (I wanna make sure I didn't screw up something here since i.e. the functions still take the entirety of x as an argument)
+
+# now let's build up from first principles how to solve this with newton's method
+
+# in 1-d scalar form, we can derive newton's method like so
+
+# f(x) ~= f(x_n) + f'(x_n) * (x - x_n) where x_n is the point we expand about (i.e. initial guess)
+
+# then we just need to solve f(x_n) + f'(x_n) * (x - x_n) = 0, which we can get with rearranging
+
+# x = x_n - f(x_n) / f'(x_n)
+
+# and then we just apply that iteratively
+
+# now let's go to n-d, starting in scalar terms (with x and x_n vectors, x_n is the initial guess and x-i is the ith component of x)
+
+# we use the multivariate chain rule
+
+# f1(x) ~= f1(x_n) + del f1 / del x-1 (x_n) * (x-1 - x_n-1) + del f1 / del x-2 (x_n) * (x-2 - x_n-2) + ... del f1 / del x-d (x_n) * (x-d - x_n-d)
+# ...
+# fd(x) ~= ...
+
+# notice we can rewrite this as (with . as the dot product)
+
+# f1(x) ~= f1(x_n) + (grad f_1 ) . (x-x_n)
+# ...
+# fd(x) ~= fd(x_n) + (grad f_d ) . (x-x_n)
+
+
+# then remember that matrix-vector multiplication M @ v can be written as
+
+# M @ v = [M_1- . v, M_2- . v, ... M_d- . v]^T
+
+# where M_i- is the ith row of M
+
+# so if we define the matrix
+
+# J(x) = [grad f1(x), grad f2(x), ..., grad fd(x)], where grad f1(x) is the first row of our matrix, grad f2(x) is the second row, etc.
+
+# but recall the Jacobian matrix of f(x) is precisely
+
+# [ del f1 / del x1, del f1 del x2...]
+# [ del f2 / del x2, ...]
+# ...
+# [...]
+
+# so J(x) is precisely the Jacobian matrix
+
+# then we can finally write our approximation of f in vectorial form
+
+# f(x) ~= f(x_n) + J(x_n) @ (x - x_n) where J(x_n) is the jacobian matrix evaluated at x_n
+
+# then we want to find
+
+# f(x_n) + J(x_n) @ (x - x_n) = 0
+
+# which we can find with
+
+# x = x_n - J^-1(x_n) @ f(x_n)
+
+# where J^-1 is the inverse of the jacobian matrix
+
+# though practically, it's easier to just solve
+
+# J(x_n) @ (x-x_n) = -f(x_n)
+
+# for (x-x_n) with methods other than inversion, and then extract x by adding x_n
+
+
+# ---
+
+# now applying this to our problem
+
+# g(x) = 0
+
+# with g(x) = (h/2) * f(x) + y_n - x
+
+# then
+
+# J_g(x_n) = (h/2) * J_f(x_n)  - I_d (where I_d is the dxd Identity matrix)
+
+# then we get
+
+# x = x_n - J_g^-1(x_n) @ g(x_n)
+
+# though practically, it's easier to just solve
+
+# J_g(x_n) @ (x-x_n) = -g(x_n)
+
+# for (x-x_n) with methods other than inversion, and then extract x by adding x_n
+
+# which we do successively until magnitude of g(x_n) is zero to within our tolerance, and thus we have our Y_1 vector finally
+
+# at which point out update is trivially
+
+# y_n+1 = y_n + h * f(Y_1)
+
+
+# Stage 1 gauss legendre
+def implicit_midpoint(y_k, h: np.floating, f: ODEFunc) -> npt.NDArray:
+    y_k = enforce_1d(y_k)
+
+    def g(x):
+        return (h / 2) * f(x) + y_k - x
+
+    # Y_1 = newtons_method_approx(g, 1e-5, y_k)
+
+    # return y_k + h * f(Y_1)
+
+    pass
+
+
+def newtons_method_exact1d(the_function, the_derivative, iterations, initial_guess):
     # f(x) ~= f(x0) + f'(x0) (x-x0) -> zero at f(x0) + f'(x0) (x-x0) = 0 -> x = x0 - f(x0)/f'(x0)
     xn = initial_guess
     for i in range(iterations):
@@ -63,7 +220,7 @@ def newtons_method_exact(the_function, the_derivative, iterations, initial_guess
     return xn
 
 
-def newtons_method_approx(the_function, initial_guess, tolerance=1e-5, maxiters=100):
+def newtons_method_approx1d(the_function, initial_guess, tolerance=1e-5, maxiters=100):
     # f(x) ~= f(x0) + f'(x0) (x-x0) -> zero at f(x0) + f'(x0) (x-x0) = 0 -> x = x0 - f(x0)/f'(x0)
     xn = initial_guess
     thefunc = the_function(xn)
